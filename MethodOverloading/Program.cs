32b7cb31ae3c45687f490aec27e481133005ff51
Addishiwot Ali <sosenamillion@yahoo.com>
2025-07-09 13:27:06 -0700

    finished Method_overloading_exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,35 @@
 {
     public class Program
     {
+        public static int Add(int numOne, int numtwo)
+        {
+            return numOne + numtwo;
+        }
+
+        public static decimal Add(decimal numOne, decimal numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static string Add(int numOne, int numTwo, bool iscurrency)
+        {
+            var sum = numOne + numTwo;
+            if (iscurrency && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (iscurrency && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(1, 1, false));
         }
     }
 }
